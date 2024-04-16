94b169263e1c72ed603420528a95749ec5e2ec91
aarellano212 <92806943+aarellano212@users.noreply.github.com>
2024-04-16 13:54:38 -0700

    I added a new button file

--- app/src/main/java/com/example/firstapp/TicTacToe.java
+++ app/src/main/java/com/example/firstapp/TicTacToe.java
@@ -8,15 +8,15 @@ import android.widget.Button
 import android.widget.TextView
 
 public class : AppCompatActivity(), View.OnClickListener {
-    var playerOneActive = false
-    private var playerOneScore: TextView? = null
-    private var playerTwoScore: TextView? = null
-    private var playerStatus: TextView? = null
-    private val buttons = arrayOfNulls<Button>(9)
-    private var reset: Button? = null
-    private var playagain: Button? = null
-    var gameState = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
-    var winningPositions = arrayOf(
+        var playerOneActive = false
+private var playerOneScore: TextView? = null
+private var playerTwoScore: TextView? = null
+private var playerStatus: TextView? = null
+private val buttons = arrayOfNulls<Button>(9)
+private var reset: Button? = null
+private var playagain: Button? = null
+        var gameState = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
+        var winningPositions = arrayOf(
         intArrayOf(0, 1, 2),
         intArrayOf(3, 4, 5),
         intArrayOf(6, 7, 8),
@@ -25,11 +25,11 @@ public class : AppCompatActivity(), View.OnClickListener {
         intArrayOf(2, 5, 8),
         intArrayOf(0, 4, 8),
         intArrayOf(2, 4, 6)
-    )
-    var rounds = 0
-    private var playerOneScoreCount = 0
-    private var playerTwoScoreCount = 0
-    override fun onCreate(savedInstanceState: Bundle?) {
+        )
+        var rounds = 0
+private var playerOneScoreCount = 0
+private var playerTwoScoreCount = 0
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         playerOneScore = findViewById<TextView>(R.id.score_Player1)
@@ -47,78 +47,78 @@ public class : AppCompatActivity(), View.OnClickListener {
         buttons[7] = findViewById<Button>(R.id.btn7)
         buttons[8] = findViewById<Button>(R.id.btn8)
         for (i in buttons.indices) {
-            buttons[i].setOnClickListener(this)
+        buttons[i].setOnClickListener(this)
         }
         playerOneScoreCount = 0
         playerTwoScoreCount = 0
         playerOneActive = true
         rounds = 0
-    }
+        }
 
-    override fun onClick(view: View) {
+        override fun onClick(view: View) {
         if ((view as Button).text.toString() != "") {
-            return
+        return
         } else if (checkWinner()) {
-            return
+        return
         }
         val buttonID = view.getResources().getResourceEntryName(view.getId())
         val gameStatePointer = buttonID.substring(buttonID.length - 1, buttonID.length).toInt()
         if (playerOneActive) {
-            view.text = "X"
-            view.setTextColor(Color.parseColor("#ffc34a"))
-            gameState[gameStatePointer] = 0
+        view.text = "X"
+        view.setTextColor(Color.parseColor("#ffc34a"))
+        gameState[gameStatePointer] = 0
         } else {
-            view.text = "O"
-            view.setTextColor(Color.parseColor("#70fc3a"))
-            gameState[gameStatePointer] = 1
+        view.text = "O"
+        view.setTextColor(Color.parseColor("#70fc3a"))
+        gameState[gameStatePointer] = 1
         }
         rounds++
         if (checkWinner()) {
-            if (playerOneActive) {
-                playerOneScoreCount++
-                updatePlayerScore()
-                playerStatus!!.text = "Player-1 has won"
-            } else {
-                playerTwoScoreCount++
-                updatePlayerScore()
-                playerStatus!!.text = "Player-2 has won"
-            }
-        } else if (rounds == 9) {
-            playerStatus!!.text = "No Winner"
+        if (playerOneActive) {
+        playerOneScoreCount++
+        updatePlayerScore()
+        playerStatus!!.text = "Player-1 has won"
         } else {
-            playerOneActive = !playerOneActive
+        playerTwoScoreCount++
+        updatePlayerScore()
+        playerStatus!!.text = "Player-2 has won"
+        }
+        } else if (rounds == 9) {
+        playerStatus!!.text = "No Winner"
+        } else {
+        playerOneActive = !playerOneActive
         }to
         reset!!.setOnClickListener {
-            playAgain()
-            playerOneScoreCount = 0
-            playerTwoScoreCount = 0
-            updatePlayerScore()
+        playAgain()
+        playerOneScoreCount = 0
+        playerTwoScoreCount = 0
+        updatePlayerScore()
         }
         playagain!!.setOnClickListener { playAgain() }
-    }
+        }
 
-    private fun checkWinner(): Boolean {
+private fun checkWinner(): Boolean {
         var winnerResults = false
         for (winningPositions in winningPositions) {
-            if (gameState[winningPositions[0]] == gameState[winningPositions[1]] && gameState[winningPositions[1]] == gameState[winningPositions[2]] && gameState[winningPositions[0]] != 2) {
-                winnerResults = true
-            }
+        if (gameState[winningPositions[0]] == gameState[winningPositions[1]] && gameState[winningPositions[1]] == gameState[winningPositions[2]] && gameState[winningPositions[0]] != 2) {
+        winnerResults = true
+        }
         }
         return winnerResults
-    }
+        }
 
-    private fun playAgain() {
+private fun playAgain() {
         rounds = 0
         playerOneActive = true
         for (i in buttons.indices) {
-            gameState[i] = 2
-            buttons[i]!!.text = ""
+        gameState[i] = 2
+        buttons[i]!!.text = ""
         }
         playerStatus!!.text = "Status"
-    }
+        }
 
-    private fun updatePlayerScore() {
+private fun updatePlayerScore() {
         playerOneScore!!.text = Integer.toString(playerOneScoreCount)
         playerTwoScore!!.text = Integer.toString(playerTwoScoreCount)
-    }
-}
+        }
+        }
